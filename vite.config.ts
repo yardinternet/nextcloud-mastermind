dbce35826c5c784177b6bc3d54b11dd270a972aa
Marc Schwartz <marc@yard.nl>
2026-03-19 13:48:43 +0100

    Fix packages
    * Built using Node 22 instead
    * Add missing

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { createAppConfig } from '@nextcloud/vite-config'
 import { join, resolve } from 'path'
+import eslint from "vite-plugin-eslint";
+import stylelint from "vite-plugin-stylelint";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export default createAppConfig(
 	{
-		main: resolve(join('src', 'main.ts')),
+		main: resolve(join("src", "main.ts")),
 	},
 	{
+		config: {
+			css: {
+				modules: {
+					localsConvention: "camelCase",
+				},
+			},
+			plugins: [eslint(), stylelint()],
+		},
+		inlineCSS: { relativeCSSInjection: true },
+		minify: isProduction,
 		createEmptyCSSEntryPoints: true,
 		extractLicenseInformation: {},
 	},
